cbbecbcaaaf7682292e34f965ac600c1ff1f9444
ymmot239 <77180103+ymmot239@users.noreply.github.com>
2022-05-05 10:14:39 -0500

    Logic.py update
    Keeps track of time, day of the week, and number of weeks
    tested up to week 6

--- logic.py
+++ logic.py
@@ -14,19 +14,49 @@ class Timer():
     def __init__(self):
         self.times = [630, 645, 715, 745, 800, 830, 1000, 1145, 1330, 1400, 1545, 1700, 1800, 1900, 2000, 2100, 2200,
         2300, 2400, 100, 200, 300, 400, 500]
-        self.pointer = 0
+        self.days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        self.timePointer = 0
+        self.dayPointer = 0
+        self.weeks = 0
+
+    def getTime(self):
+        return self.times[self.timePointer]
 
     def getTimeString(self):
-        hour = self.times[self.pointer]//100
-        minute = self.times[self.pointer]%100
+        week = ""
+        day = self.days[self.dayPointer]
+        hour = self.times[self.timePointer]//100
+        minute = self.times[self.timePointer]%100
         ampm = "am"
-        if hour > 12:
+        if (hour > 12) and (hour <24):
             hour -=12
             ampm = "pm"
         if minute == 0:
-            minute == "00"
-        return str(hour) + ":" + str(minute) + " " + ampm
+            minute = "00"
+        if self.weeks >0:
+            week = "Week " + str(self.weeks+1)
+        return week + " " + day + ", " + str(hour) + ":" + str(minute) + " " + ampm
 
     def incrementTime(self):
-        self.pointer+=1
+        self.timePointer+=1
+        if self.timePointer > len(self.times)-1:
+            self.timePointer = 0
+        
+        if self.times[self.timePointer] == 2400:
+            self.dayPointer+=1
 
+        if self.dayPointer > len(self.days)-1:
+            self.dayPointer = 0
+            self.weeks+=1
+
+    def nextDay(self):
+        if (self.times[self.timePointer] < 630) or self.times[self.timePointer]==2400:
+            pass
+        else:
+            self.dayPointer+=1
+            if self.dayPointer > len(self.days)-1:
+                self.dayPointer = 0
+                self.weeks+=1
+        self.timePointer = 0
+        
+        
