0c6741761bb2e0be8a7f6e2e52f8e5a80cea4a2c
ymmot239 <77180103+ymmot239@users.noreply.github.com>
2022-05-06 11:34:02 -0500

    Create manage.py
    a class to manage the bars, timer, and choices classes
    allows the game to be played in a text-based version

---
+++ manage.py
@@ -0,0 +1,63 @@
+import timer
+import choices
+import bars
+
+class Manager():
+
+    def __init__(self):
+        self.timer = timer.Timer()
+        self.choice = choices.Choices()
+        self.bar = bars.Bars()
+        self.absence = 0
+
+    def getTime(self):
+        return self.timer.getTimeString()
+
+    def getValues(self):
+        return self.bar.get()
+
+    def getChoices(self):
+        if ("Saturday" in self.timer.getTimeString()) or ("Sunday" in self.timer.getTimeString()):
+            return self.choice.getChoicesWeekend(self.timer.getTime())
+        else:
+            return self.choice.getChoices(self.timer.getTime())
+
+    def makeChoice(self, choice):
+        change = self.choice.getResult(choice)
+
+        outs = self.bar.get()
+        if (outs[1] < 30) or (outs[2] < 30): #additonal sanity for hunger and exhaustion
+            change[0]+=2
+        if (outs[1] > 70) or (outs[2] > 70):
+            change[0]-=5
+
+        self.bar.change(change[0],change[1],change[2]) #make changes based on your choices
+
+        outs = self.bar.get()
+        adjust = [0,0,0]
+        for x in range(3): # cap function
+            if outs[x] > 100:
+                adjust[x] = 100-outs[x]
+            if outs[x]<0:
+                adjust[x] = -outs[x]
+        self.bar.change(adjust[0],adjust[1],adjust[2])
+
+        if (choice == "sleep"):
+            print("insert dream game here")
+            if self.timer.getTime() == 1330: 
+                self.timer.incrementTime()   #if you sleep (not at lunch), you sleep until the next day
+            else:
+                self.timer.nextDay()
+            self.bar.change(0,0,-20)
+        else:
+            self.timer.incrementTime()
+
+        if choice == "no": #absences and makeups
+            self.absence+=1
+        if choice == "absence makeup":
+            self.absence-=1
+
+        self.bar.change(0,2,2) #additional hunger and exhaustion per turn
+        outs = self.bar.get()
+        outs.append(self.absence)
+        return outs
